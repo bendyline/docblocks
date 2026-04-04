96e8f1c53a1c9640c991719880f37c0561cde88f
Mike Ammerlaan <mike@bendyline.com>
2026-04-03 18:02:18 -0700

    fix: Publish updates

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
+  splitting: false,
   dts: true,
   sourcemap: true,
   clean: true,
